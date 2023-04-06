b58a3b52acb3cce855f555e43f691d95c3336a6a
unknown <ian.pinho.tsis@gmail.com>
2023-04-06 11:23:08 -0300

    Third exercise done, calculated the average of 4 grades

---
+++ Exercicio-3_Lista.py
@@ -0,0 +1,7 @@
+NotaEtapa1 = 7
+NotaEtapa2 = 9
+NotaEtapa3 = 8
+NotaEtapa4 = 10
+print ("Notas de cada etapa:", NotaEtapa1, NotaEtapa2, NotaEtapa3, NotaEtapa4)
+Media = (NotaEtapa1+NotaEtapa2+NotaEtapa3+NotaEtapa4)/4
+print("Média:",Media)
