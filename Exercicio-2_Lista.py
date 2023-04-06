3c010709e920f099a3417bf548546f2a7dd67aff
unknown <ian.pinho.tsis@gmail.com>
2023-04-06 11:10:27 -0300

    Second exercise done, inverted the order of numbes in a row

---
+++ Exercicio-2_Lista.py
@@ -0,0 +1,5 @@
+NumerosInteiros = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+PopVar = NumerosInteiros.pop(),NumerosInteiros.pop(),NumerosInteiros.pop(),NumerosInteiros.pop(),NumerosInteiros.pop(),NumerosInteiros.pop(),NumerosInteiros.pop(),NumerosInteiros.pop(),NumerosInteiros.pop(),NumerosInteiros.pop()
+ListaNova = PopVar
+print(ListaNova)
+
