82fd168a8fc75d816fe5abf9cc3dc6867d403e9f
unknown <ian.pinho.tsis@gmail.com>
2023-04-13 15:00:02 -0300

    8. Faça um Programa que peça a idade e a altura de 5 pessoas, armazene cada informação no seu respectivo vetor. Imprima a idade e a altura na ordem inversa a ordem lida.

--- Exercicio-7_Lista.py
+++ Exercicio-7_Lista.py
@@ -1,4 +1,6 @@
 listaInt = [1,2,3,4,5,]
+
+
 soma=sum(listaInt)
 print("Soma:",soma)
 multiplicação = listaInt[0]*listaInt[1]*listaInt[2]*listaInt[3]*listaInt[4]
---
+++ Exercicio-8_Lista.py
@@ -0,0 +1,15 @@
+altura = []
+idade = []
+
+for i in range (5):
+    print("Insira sua Altura")
+    altura.append(float(input()))
+    print("Insira sua idade")
+    idade.append(int(input()))
+
+altura.reverse()
+idade.reverse()
+
+print (altura)
+print (idade)
+
