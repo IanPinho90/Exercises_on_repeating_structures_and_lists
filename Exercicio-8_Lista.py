59f470e01a9dad4828a136c2c1d8676b91fe3e81
unknown <ian.pinho.tsis@gmail.com>
2023-04-13 15:33:32 -0300

    9. Faça um Programa que leia um vetor A com 10 números inteiros, calcule e mostre a soma dos quadrados dos elementos do vetor.

--- Exercicio-8_Lista.py
+++ Exercicio-8_Lista.py
@@ -2,9 +2,9 @@ altura = []
 idade = []
 
 for i in range (5):
-    print("Insira sua Altura")
+    print("Insira sua Altura:")
     altura.append(float(input()))
-    print("Insira sua idade")
+    print("Insira sua idade:")
     idade.append(int(input()))
 
 altura.reverse()
