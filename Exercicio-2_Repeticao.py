166f4b859f2ba6febf078689aad785d13cfcd394
unknown <ian.pinho.tsis@gmail.com>
2023-04-14 10:38:08 -0300

    2.Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.

--- Exercicio-2_Repeticao.py
+++ Exercicio-2_Repeticao.py
@@ -1,4 +1,14 @@
+Parada=False
 print("Insira seu nome de usuário:")
 userName=input()
+print ("Usuário é:",userName)
 print("Insira sua senha:")
 senha=input()
+while Parada==False:
+    if senha==userName:
+        print("Senha não pode ser igual o nome.")
+        print("Insira sua senha:")
+        senha=input()
+    else:
+        print("Conta criada com sucesso.")
+        Parada=True
